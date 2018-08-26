429bb16a5d757b92a667bd13f73c36753f8cecf6
hongduclamvt@gmail.com <bitbucket!@#$>
2018-08-26 20:35:20 +0700

    Add script to delpoy

--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -10,27 +10,33 @@ class Welcome extends PureComponent {
   }
 
   componentDidMount() {
-    // MessengerExtensions.getSupportedFeatures(function success(result) {
-    //   let features = result.supported_features;
-    //   if (features.indexOf("context") != -1) {
-    //     window.MessengerExtensions.getContext(FB_APP_ID,
-    //       function success(thread_context) {
-    //         document.getElementById('psidValue').innerHTML = `Messenger ID: ${thread_context.psid}`;
-    //       },
-    //       function error(err) {
-    //         document.getElementById('psidValue').innerHTML = `ERROR: ${err}`;
-    //       }
-    //     );
-    //   }
-    //
-    // }, function error(err) {
-    //   document.getElementById('psidValue').innerHTML = `ERROR: ${err}`;
-    // });
-    window.MessengerExtensions.getUserID(function success(uids) {
-      document.getElementById('psidValue').innerHTML = `Messenger ID: ${uids.psid}`;
-    }, function error(err) {
-      document.getElementById('psidValue').innerHTML = `ERROR: ${err}`;
-    });
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/messenger.Extensions.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'Messenger'));
+    window.extAsyncInit = function () {
+      window.MessengerExtensions.getSupportedFeatures(function success(result) {
+        let features = result.supported_features;
+        if (features.indexOf("context") != -1) {
+          window.MessengerExtensions.getContext(FB_APP_ID,
+            function success(thread_context) {
+              document.getElementById('psidValue').innerHTML = `Messenger ID: ${thread_context.psid}`;
+            },
+            function error(err) {
+              document.getElementById('psidValue').innerHTML = `ERROR: ${err}`;
+            }
+          );
+        }
+      }, function error(err) {
+        document.getElementById('psidValue').innerHTML = `ERROR: ${err}`;
+      });
+    };
   }
 
   render() {
