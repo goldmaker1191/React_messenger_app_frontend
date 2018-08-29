9bf0a0331972326e52c772650693a0893efaaaea
HONG Duc Lam <ty333457@toyo-local.co.jp>
2018-08-29 09:44:49 +0700

    udpate

--- config/env.js
+++ config/env.js
@@ -8,6 +8,9 @@ const paths = require('./paths');
 delete require.cache[require.resolve('./paths')];
 
 const NODE_ENV = process.env.NODE_ENV;
+
+console.log('process.env HEROKU:', process.env);
+
 if (!NODE_ENV) {
   throw new Error(
     'The NODE_ENV environment variable is required but was not specified.'
